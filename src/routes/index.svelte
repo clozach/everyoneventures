3faa157a70f33e3a363cf5bfa4f15f170a3e67e4
Chris Lozac'h <clozach@gmail.com>
2019-10-10 01:02:04 -0700

    Duplicated "Recent Posts" functionality from the blog index to the main index.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,17 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`blog.json`)
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
+<script>
+  export let posts;
+</script>
+
 <style>
   h1,
   figure,
@@ -50,3 +64,17 @@
     Try editing this file (src/routes/index.svelte) to test live reloading.
   </strong>
 </p>
+
+<h1>Recent posts</h1>
+
+<ul>
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+    <li>
+      <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
+</ul>
