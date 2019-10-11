637c99dbc93e8dfd4b602f19cb1618fadb2161e6
Chris Lozac'h <clozach@gmail.com>
2019-10-10 20:12:01 -0700

    Added typewriter effect to the Ouroboros sub-title, 2 sec after display

--- src/routes/ouroboros/index.svelte
+++ src/routes/ouroboros/index.svelte
@@ -1,3 +1,28 @@
+<script>
+  function typewriter(node, { speed = 50, delay = 0 }) {
+    const valid =
+      node.childNodes.length === 1 && node.childNodes[0].nodeType === 3;
+
+    if (!valid) {
+      throw new Error(
+        `This transition only works on elements with a single text node child`
+      );
+    }
+
+    const text = node.textContent;
+    const duration = text.length * speed;
+
+    return {
+      delay,
+      duration,
+      tick: t => {
+        const i = ~~(text.length * t);
+        node.textContent = text.slice(0, i);
+      }
+    };
+  }
+</script>
+
 <style>
   h1 {
     margin-top: 13rem;
@@ -10,4 +35,4 @@
 </style>
 
 <h1>Ouroboros</h1>
-<h2>The story that tells itself</h2>
+<h2 in:typewriter={{ delay: 2000 }}>The story that tells itself</h2>
