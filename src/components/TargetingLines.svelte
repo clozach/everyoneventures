dfd44611d4d8d89c02cabd7949fa0544cdb06ca8
Chris Lozac'h <clozach@gmail.com>
2019-11-10 23:02:32 -0800

    🐞 Minor bug fix: eliminated attribute error on svg lines
    Errors were coming up from attempting to render the lines before getting the window measurements.

--- src/components/TargetingLines.svelte
+++ src/components/TargetingLines.svelte
@@ -9,47 +9,49 @@
 
 <svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 
-<svg width="100%" height="100%" style="position:absolute; top:0;">
-  <line
-    x1="0"
-    x2={width}
-    y1="0"
-    y2={height}
-    stroke={lineColor}
-    stroke-width={lineStroke} />
-  <line
-    x1="0"
-    x2={width}
-    y1={height}
-    y2="0"
-    stroke={lineColor}
-    stroke-width={lineStroke} />
-  <line
-    x1="0"
-    x2={width}
-    y1={height / lh}
-    y2={height / lh}
-    stroke={lineColor}
-    stroke-width={lineStroke} />
-  <line
-    x1="0"
-    x2={width}
-    y1={height - height / lh}
-    y2={height - height / lh}
-    stroke={lineColor}
-    stroke-width={lineStroke} />
-  <line
-    x1={width / lh}
-    x2={width / lh}
-    y1="0"
-    y2={height}
-    stroke={lineColor}
-    stroke-width={lineStroke} />
-  <line
-    x1={width - width / lh}
-    x2={width - width / lh}
-    y1="0"
-    y2={height}
-    stroke={lineColor}
-    stroke-width={lineStroke} />
-</svg>
+{#if width}
+  <svg width="100%" height="100%" style="position:absolute; top:0;">
+    <line
+      x1="0"
+      x2={width}
+      y1="0"
+      y2={height}
+      stroke={lineColor}
+      stroke-width={lineStroke} />
+    <line
+      x1="0"
+      x2={width}
+      y1={height}
+      y2="0"
+      stroke={lineColor}
+      stroke-width={lineStroke} />
+    <line
+      x1="0"
+      x2={width}
+      y1={height / lh}
+      y2={height / lh}
+      stroke={lineColor}
+      stroke-width={lineStroke} />
+    <line
+      x1="0"
+      x2={width}
+      y1={height - height / lh}
+      y2={height - height / lh}
+      stroke={lineColor}
+      stroke-width={lineStroke} />
+    <line
+      x1={width / lh}
+      x2={width / lh}
+      y1="0"
+      y2={height}
+      stroke={lineColor}
+      stroke-width={lineStroke} />
+    <line
+      x1={width - width / lh}
+      x2={width - width / lh}
+      y1="0"
+      y2={height}
+      stroke={lineColor}
+      stroke-width={lineStroke} />
+  </svg>
+{/if}
